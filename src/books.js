f50259e788fa4e55478956f6977d3eaa8af93497
Vasiliu Diana Elena <diana.vasiliu10@gmail.com>
2021-10-20 09:14:01 +0300

    Improved fetch

--- src/books.js
+++ src/books.js
@@ -1,20 +1,15 @@
 import fetch from 'node-fetch'
 
 const books = async (req, res) => {
-    fetch(
+    const response = await fetch(
         'http://libgen.rs/json.php?ids=1,2,3,4,6,7,8,9&fields=Title,Author,Year,Pages,Id'
     )
-        .then((response) => {
-            return response.json()
-        })
-        .then((body) => {
-            const books = body.map(({ title, author }) => ({
-                title,
-                author,
-            }))
-
-            res.send(books)
-        })
+    const body = await response.json()
+    const books = body.map(({ title, author }) => ({
+        title,
+        author,
+    }))
+    res.send(books)
 }
 
 export default books
